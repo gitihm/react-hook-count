7b68bc32ed5f612750503b8d96d10c1b340fee87
air <ismael897@outlook.com>
2019-07-15 16:33:27 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import { async } from 'q';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [count , setCount] = useState(0)
+  const [data , setData] = useState([])
+  const [brand , setBrand] = useState('')
+  const [price , setPrice] = useState(0)
+  return (  
+  <div>
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo center">react-hook-demo</a>
+      </div>
+    </nav>
+    <div className="middle">
+      <h1>{count}</h1>
+      <a className="waves-effect waves-light btn" onClick={()=>{setCount(count+1)}}>Add count</a>
+      <div style={{marginTop:'10px'}}>
+        <div style={{marginLeft:'40%',marginRight:'40%'}}>
+          <input name='brand' type="text" onChange={(e)=>{setBrand(e.target.value)}}/>
+          <input name='price' type="number"  onChange={(e)=>{setPrice(e.target.value)}}/>
+        </div>
+        <a className="waves-effect waves-light btn" onClick={()=>{setData([...data,{id:data.length,brand:brand,price:price}])}} >Add item</a>
+        
+        { data.map( item => (
+          <h3 key={item.id}>{item.brand} {item.price}</h3>
+        ))}
+        
+      </div>
     </div>
+  </div>
   );
 }
 
